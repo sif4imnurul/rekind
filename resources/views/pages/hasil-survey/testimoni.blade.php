f5e4ce75d295ce22c7f5f41e737a25e91766eb17
Alifa Salsabila <153807422+alipa97@users.noreply.github.com>
2025-05-06 12:14:49 +0700

    Add files via upload

---
+++ resources/views/pages/hasil-survey/testimoni.blade.php
@@ -0,0 +1,195 @@
+@extends('layouts.employee')
+
+@section('title', 'Hasil Survey | Rekind')
+
+@section('content')
+
+<div class="w-full px-4 md:px-25 py-6 overflow-x-hidden ">
+    <div class="max-w-full flex justify-between items-start">
+        <!-- Kiri -->
+        <div class="w-[662px] overflow-hidden flex flex-col justify-start items-start gap-[10px]">
+            <div class="text-[#1D3A6D] text-[24px] font-bold leading-[32px] tracking-[0.10px] font-montserrat">Hasil Survey</div>
+            <div class="text-[#737373] text-[14px] font-normal leading-[20px] tracking-[0.20px] font-montserrat">Analisis Data dan Wawasan dari Responden</div>
+        </div>
+
+        
+    </div>
+
+    <!-- line -->
+    <div class="py-6">
+        <hr class="border-[2px] text-[var(--line)] rounded-2xl">
+    </div>
+
+    <div class="grid grid-cols-2 md:grid-cols-4 gap-4 w-full py-2">
+        <!-- Button 1 -->
+        <div class="h-16 flex justify-center items-center">
+            <div class="w-full h-full bg-white rounded-[30px] outline-1 outline-[var(--button)] flex justify-center items-center px-3">
+                <a href="#" class="text-[var(--sub-text)] text-base font-normal text-center">
+                    Brand Awareness
+                </a>
+            </div>
+        </div>
+
+        <!-- Button 2 -->
+        <div class="h-16 flex justify-center items-center">
+            <div class="w-full h-full bg-white rounded-[30px] outline-1 outline-[var(--button)] flex justify-center items-center px-3">
+                <a href="#" class="text-[var(--sub-text)] text-base font-normal text-center">
+                    Brand Positioning
+                </a>
+            </div>
+        </div>
+
+        <!-- Button 3 -->
+        <div class="h-16 flex justify-center items-center">
+            <div class="w-full h-full bg-[var(--button)] rounded-[30px] outline-1 outline-[var(--button)] flex justify-center items-center px-3">
+                <a href="#" class="text-white text-base font-normal text-center">
+                    Customer Satisfaction & Engagement
+                </a>
+            </div>
+        </div>
+
+        <!-- Button 4 -->
+        <div class="h-16 flex justify-center items-center">
+            <div class="w-full h-full bg-white rounded-[30px] outline-1 outline-[var(--button)] flex justify-center items-center px-3">
+                <a href="#" class="text-[var(--sub-text)] text-base font-normal text-center">
+                    SKL (Survey Kepuasan Layanan)
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-6 w-full h-full flex flex-col md:flex-row items-center justify-between gap-y-4 md:gap-[30px]">
+        <!-- Kiri: Search Box -->
+        <div class="w-full max-w-[510px] h-[62px] flex items-center gap-[10px]">
+            <div class="w-full h-[56px] min-w-[280px] max-w-[720px] bg-[var(--highlight-text-box)] overflow-hidden rounded-full flex items-center justify-center gap-[4px]">
+                <div class="flex-1 h-full flex items-center gap-[4px] px-[4px]">
+                    <div class="w-[22px] h-[48px] inline-flex flex-col items-center justify-center gap-[10px]"></div>
+                    <div class="flex-1 h-full flex items-center justify-start gap-[10px]">
+                        <div class="text-[#737373] text-[16px] font-roboto font-normal leading-[24px] tracking-[0.5px]">
+                            Hinted search text
+                        </div>
+                    </div>
+                    <div class="flex items-center justify-end">
+                        <div class="w-[48px] h-[48px] inline-flex flex-col items-center justify-center gap-[10px]">
+                            <div class="overflow-hidden rounded-full flex items-center justify-center gap-[10px]">
+                                <div class="p-[8px] flex items-center justify-center gap-[10px]">
+                                    <div class="w-[24px] h-[24px] relative">
+                                        <div class="absolute text-[#737373]">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path></svg>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Kanan: Sort Box -->
+        <div class="w-full max-w-[510px] overflow-hidden rounded-full flex items-center justify-end gap-[4px]">
+            <div class="w-[130px] h-[56px] p-[4px] flex items-center justify-start gap-[4px]">
+            <div class="w-[22px] h-[48px] inline-flex flex-col items-center justify-center gap-[10px]"></div>
+            <div class="w-[62px] h-full flex items-center justify-start gap-[10px]">
+                <div class="text-[#737373] text-[16px] font-roboto font-normal leading-[24px] tracking-[0.5px]">
+                Sort by
+                </div>
+            </div>
+            <div class="w-[16px] h-[20px] text-[#737373]">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M8 16H4l6 6V2H8zm6-11v17h2V8h4l-6-6z"></path></svg>
+            </div>
+            </div>
+            <div class="w-[24px] h-[24px] relative"></div>
+        </div>
+    </div>
+
+    <div class="w-full inline-flex justify-center items-center gap-2.5">
+        <div class="h-14 text-center justify-center text-sky-900 text-2xl font-medium leading-loose tracking-tight">Testimoni Pelanggan</div>
+    </div>
+
+    <div class=" w-full h-full rounded-[10px] inline-flex flex-col justify-center items-center gap-2.5">
+
+        <!-- Testimoni Box -->
+        <div class="w-full p-6 rounded-[20px] outline-1 outline-offset-[-1px] outline-blue-300 flex flex-col justify-start items-start gap-2">
+            <!-- Stars -->
+            <div data-ratings="5" data-stroke="True" class="w-full inline-flex justify-start items-center gap-1">
+                <div class="w-4 h-4 relative overflow-hidden">
+                    <div class="absolute text-yellow-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="w-full" height="h-full" viewBox="0 0 24 24" class="fill-current"><path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path></svg>
+                    </div>
+                </div>
+                <div class="w-4 h-4 relative overflow-hidden">
+                    <div class="absolute text-yellow-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="w-full" height="h-full" viewBox="0 0 24 24" class="fill-current"><path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path></svg>
+                    </div>
+                </div>
+                <div class="w-4 h-4 relative overflow-hidden">
+                    <div class="absolute text-yellow-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="w-full" height="h-full" viewBox="0 0 24 24" class="fill-current"><path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path></svg>
+                    </div>
+                </div>
+                <div class="w-4 h-4 relative overflow-hidden">
+                    <div class="absolute text-yellow-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="w-full" height="h-full" viewBox="0 0 24 24" class="fill-current"><path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path></svg>
+                    </div>
+                </div>
+                <div class="w-4 h-4 relative overflow-hidden">
+                    <div class="absolute text-yellow-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="w-full" height="h-full" viewBox="0 0 24 24" class="fill-current"><path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path></svg>
+                    </div>
+                </div>
+            </div>
+            <!-- Testimoni Text -->
+            <div class="w-[1003px] justify-start">
+                <span class="text-slate-400 text-3xl font-bold">“</span>
+                <span class="text-neutral-500 text-xl font-normal">Contoh testimoni.</span>
+                <span class="text-neutral-500 text-3xl font-normal"> </span>
+                <span class="text-slate-400 text-3xl font-bold">”</span>
+            </div>
+        </div>
+
+        <!-- Ulangi 3 Box Testimoni Lainnya -->
+        <div class="w-full p-6 rounded-[20px] outline-1 outline-offset-[-1px] outline-blue-300 flex flex-col justify-start items-start gap-2">
+            <!-- Stars -->
+            <div data-ratings="5" data-stroke="True" class="w-full inline-flex justify-start items-center gap-1">
+                <div class="w-4 h-4 relative overflow-hidden">
+                    <div class="absolute text-yellow-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="w-full" height="h-full" viewBox="0 0 24 24" class="fill-current"><path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path></svg>
+                    </div>
+                </div>
+                <div class="w-4 h-4 relative overflow-hidden">
+                    <div class="absolute text-yellow-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="w-full" height="h-full" viewBox="0 0 24 24" class="fill-current"><path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path></svg>
+                    </div>
+                </div>
+                <div class="w-4 h-4 relative overflow-hidden">
+                    <div class="absolute text-yellow-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="w-full" height="h-full" viewBox="0 0 24 24" class="fill-current"><path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path></svg>
+                    </div>
+                </div>
+                <div class="w-4 h-4 relative overflow-hidden">
+                    <div class="absolute text-yellow-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="w-full" height="h-full" viewBox="0 0 24 24" class="fill-current"><path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path></svg>
+                    </div>
+                </div>
+                <div class="w-4 h-4 relative overflow-hidden">
+                    <div class="absolute text-yellow-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="w-full" height="h-full" viewBox="0 0 24 24" class="fill-current"><path d="M21.947 9.179a1.001 1.001 0 0 0-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 0 0-1.822-.001L8.622 8.05l-5.701.453a1 1 0 0 0-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 0 0 1.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 0 0 1.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path></svg>
+                    </div>
+                </div>
+            </div>
+            <!-- Testimoni Text -->
+            <div class="w-[1003px] justify-start">
+                <span class="text-slate-400 text-3xl font-bold">“</span>
+                <span class="text-neutral-500 text-xl font-normal">Contoh testimoni.</span>
+                <span class="text-neutral-500 text-3xl font-normal"> </span>
+                <span class="text-slate-400 text-3xl font-bold">”</span>
+            </div>
+        </div>
+    </div>
+
+    @include('components.pagination')
+
+</div> 
+
+@endsection
