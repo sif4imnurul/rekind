e35b22bce42ffe1f85011337c061a1d65a26dd15
Alifa Salsabila <153807422+alipa97@users.noreply.github.com>
2025-05-14 12:33:34 +0700

    Update dashboard.blade.php

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -37,13 +37,13 @@
                 <i class="fas fa-bars"></i>
             </button>
             <h1 class="text-xl font-semibold">@yield('title', 'Dashboard')</h1>
-            <div class="flex items-center gap-3 text-[var(--button)]">
+            <a href="#" class="flex items-center gap-3 text-[var(--button)]">
                 <svg xmlns="http://www.w3.org/2000/svg" class="fill-current" viewBox="0 0 24 24" width="48" height="48">
                     <path
                         d="M12 2C6.579 2 2 6.579 2 12s4.579 10 10 10 10-4.579 10-10S17.421 2 12 2zm0 5c1.727 0 3 1.272 3 3s-1.273 3-3 3c-1.726 0-3-1.272-3-3s1.274-3 3-3zm-5.106 9.772c.897-1.32 2.393-2.2 4.106-2.2h2c1.714 0 3.209.88 4.106 2.2C15.828 18.14 14.015 19 12 19s-3.828-.86-5.106-2.228z">
                     </path>
                 </svg>
-            </div>
+            </a>
         </header>
 
         <!-- Page Content -->
